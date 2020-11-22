c2b8cbd227b9eeadc779a22b7967835b6e35de51
houxingyi <1026043046@qq.com>
2020-11-22 21:40:19 +0800

    3-3

--- app/controller/demo.php
+++ app/controller/demo.php
@@ -6,7 +6,28 @@ use app\BaseController;
 class Demo extends BaseController
 {
     public function show() {
-        return '111';
+        
+        $result = [
+            "status" => 1,
+            "message"  => "OK",
+            "result" => [
+                "id" => 1
+            ]
+        ];
+
+        $header = [
+            "Token" => "1dafaf"
+        ];
+
+        // 返回json格式的数据
+        return json($result, 200, $header);
+        // return '111222'; 
+    }
+
+    public function request() {
+        // 输出内容
+        dump($this -> request -> param());
+        dump($this -> request -> get());
     }
 
 }
