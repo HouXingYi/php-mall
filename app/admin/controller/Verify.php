d666aa8132a3dfd42a96ed1a1d7b9e27ea69d3fc
houxingyi <1026043046@qq.com>
2020-12-15 16:41:26 +0800

    登录接口 前后端 初步联调

--- app/admin/controller/Verify.php
+++ app/admin/controller/Verify.php
@@ -7,10 +7,8 @@ use think\captcha\facade\Captcha;
 class Verify {
 
   public function index() {
-
     // 传入配置的字符串
     return Captcha::create("verifyConfig");
-
   }
 
 }
--- public/static/admin/js/login.js
+++ public/static/admin/js/login.js
@@ -23,27 +23,34 @@ layui.use(['form'], function () {
             layer.msg('验证码不能为空');
             return false;
         }
-        // 验证码 校验
-        var val = $(".input-val").val().toLowerCase();
-        var num = show_num.join("");
-        if(val != num){
-            layer.msg('验证码错误！请重新输入！');
-            draw(show_num);
-            return false;
-        }
 
-        window.location = '../index.html';
-        return false;
+        // 验证码 校验
+        // var val = $(".input-val").val().toLowerCase();
+        // var num = show_num.join("");
+        // if(val != num){
+        //     layer.msg('验证码错误！请重新输入！');
+        //     draw(show_num);
+        //     return false;
+        // }
+
+        // window.location = '../index.html';
+        // return false;
         $(".input-val").val('');
+
+        var url = "/admin/login/check";
         $.ajax({
             url,
             data,
             type:"POST",
             success(res){
-                if(res.status == 200){
+                console.log('res', res)
+                if(res.status == 1){
                     layer.msg('登录成功', function () {
-                        window.location = '/xiaozhu/index.html';
+                        console.log('跳转！！！')
+                        window.location = '/admin/index/index';
                     });
+                } else {
+                    layer.msg(res.message);
                 }
             }
         })
