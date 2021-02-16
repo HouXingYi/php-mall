648435483d158a0eadd115d06b2c2b51ee78d2f3
houxingyi <1026043046@qq.com>
2021-02-16 23:42:41 +0800

    s

--- app/api/route/api.php
+++ app/api/route/api.php
@@ -3,7 +3,9 @@ use think\facade\Route;
 
 Route::rule("smscode", "sms/code", "POST");
 Route::resource('user', 'User');
+
 Route::rule("lists", "mall.lists/index");
+
 Route::rule("subcategory/:id", "category/sub");
 Route::rule("detail/:id", "mall.detail/index");
 
